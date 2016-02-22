99d403426b8cbf8d0b0f10bf27300dfb679cfae2
Abhinav Singh <abhiabhiabhiabhinav@gmail.com>
2016-02-23 02:44:37 +0530

    Added temporary background function and speech recognition code

--- geditPlugin/pyDictator.py
+++ geditPlugin/pyDictator.py
@@ -4,7 +4,9 @@ sys.path.append('.local/share/gedit/plugins/')
 import setlog
 logger=setlog.logger
 logger.debug('Start Plugin')
-
+import speech_recognition as sr
+import threading
+import time
 from gi.repository import GObject, Gtk, Gedit
 ui_str = """
 <ui>
@@ -39,10 +41,8 @@ class PyDictatorPlugin(GObject.Object,Gedit.WindowActivatable):
         self._remove_menu()
         self._action_group = None
         logger.debug("Finished inserting menu")
-        #('6paragraph',None, 'Log it','<Control><Alt>6','Log-it',self.log_action)
-
+        
     def _insert_menu(self):
-        #logger.debug("inside insert menu")
         actions = [
             ('PyDictator',None,'PyDictator'),
             ("Clear", None, "Clear document",'<Control><Alt>1', "Clear the document",self.on_clear_document_activate),
@@ -52,14 +52,10 @@ class PyDictatorPlugin(GObject.Object,Gedit.WindowActivatable):
         manager = self.window.get_ui_manager()
         # Create a new action group
         self._action_group = Gtk.ActionGroup("PyDictatorPluginActions")
-        #logger.debug("putting actions")        
         self._action_group.add_actions(actions)
-        #logger.debug("put done")
         # Insert the action group
-        #logger.debug("adding into actions group")
         manager.insert_action_group(self._action_group)
         # Merge the UI
-        #logger.debug("merge into ui")
         self._ui_id = manager.add_ui_from_string(ui_str)        
         
         #oldmethod        
@@ -94,4 +90,39 @@ class PyDictatorPlugin(GObject.Object,Gedit.WindowActivatable):
 
     def on_logit_activate(self, action):
         self.do_log('Executing my action')
+        #self.some_func()
+        thread=threading.Thread(target=self.always, args=())
+        thread.daemon=True;
+        thread.start()
+        
+    def always(self):
+        while True:
+            self.do_log("Debug Infinite Background Function")
+
+    def callback(recognizer, audio):
+        # received audio data, now we'll recognize it using Google Speech Recognition
+        logger.debug("In Callback function")
+        try:
+            text_out=recognizer.recognize_sphinx(audio)
+            print("Sphinx Speech Recognition thinks you said " +text_out )
+            if text_out=='hello':
+                logger.debug('something')
+        except sr.UnknownValueError:
+            print("Sphinx Speech Recognition could not understand audio")
+        except sr.RequestError as e:
+            print("Could not request results from Sphinx Speech Recognition service; {0}".format(e))
+    
+    def recognize_(self):
+        r = sr.Recognizer()
+        m = sr.Microphone()
+        with m as source:
+            r.adjust_for_ambient_noise(source) # we only need to calibrate once, before we start listening
+        # start listening in the background (note that we don't have to do this inside a `with` statement)
+        stop_listening = r.listen_in_background(m, self.callback)
+        # `stop_listening` is now a function that, when called, stops background listening
+
+        # do some other computation for 5 seconds, then stop listening and keep doing other computations
+        for _ in range(5): time.sleep(0.1) # we're still listening even though the main thread is doing other things
+        stop_listening() # calling this function requests that the background listener stop listening
+        while True: time.sleep(0.1)
 
