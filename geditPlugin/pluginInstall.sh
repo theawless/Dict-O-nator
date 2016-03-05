261a5496e1495408c53b73e7d88ce0553f9eefb6
Abhinav Singh <abhiabhiabhiabhinav@gmail.com>
2016-03-05 23:06:03 +0530

    Added the install.sh for gedit plugin

--- geditPlugin/pluginInstall.sh
+++ geditPlugin/pluginInstall.sh
@@ -1,10 +1,10 @@
+#!/bin/sh
 
-
-"Updating"
+echo "Updating"
 sudo apt-get update
 sudo apt-get upgrade
 
-"Install Dependencies"
+echo "Install Dependencies"
 sudo apt-get install python3-pip
 sudo apt-get install swig
 sudo apt-get install portaudio-dev puthon-all-dev python3-all-dev
@@ -12,14 +12,14 @@ sudo pip3 install pyaudio
 sudo pip3 install SpeechRecognition
 sudo pip3 install pocketsphinx
 
-"Make Directory"
+echo "Making Directory"
 sudo mkdir ~/.local/share/gedit/plugins/
 
-"Move Files"
+echo "Moving Files"
 cp pyDictator.plugin ~/.local/share/gedit/plugins/
 cp pyDictator.py ~/.local/share/gedit/plugins/
 cp recogSpeech.py ~/.local/share/gedit/plugins/
 cp setlog.py ~/.local/share/gedit/plugins/
 cp statesMod.py ~/.local/share/gedit/plugins/
 
-"Finished"
+echo "Finished"
